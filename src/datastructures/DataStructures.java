ef0e23a30a3b81a463f3bc9a22fca385a5c8f3ab
Moses Nyambu <nyamosekitando@gmail.com>
2020-05-21 15:59:54 +0300

    initial commit

---
+++ src/datastructures/DataStructures.java
@@ -0,0 +1,13 @@
+package datastructures;
+
+public class DataStructures {
+
+    public static void main(String[] args) {
+        Arrays num = new Arrays(3);
+        num.insertElement(10);
+        num.insertElement(20);
+        num.insertElement(30);
+        num.insertElement(40);
+        num.printArray();
+    }
+}
