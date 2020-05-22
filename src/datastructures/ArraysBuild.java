3b40bdd0022a1f5e999b805fd01255748cabad76
Moses Nyambu <nyamosekitando@gmail.com>
2020-05-22 10:07:32 +0300

    array insert and delete at index

---
+++ src/datastructures/ArraysBuild.java
@@ -0,0 +1,52 @@
+package datastructures;
+
+import java.util.*;
+
+public class ArraysBuild {
+
+    private int[] items;
+    private int count = 0;
+    private int i = 0;
+
+    public ArraysBuild(int length) {
+        items = new int[length];
+    }
+
+    public void printArray() {
+        System.out.println(Arrays.toString(items));
+    }
+
+    public void insertElement(int item) {
+        if (count >= items.length) {
+            int[] list = new int[items.length + 1];
+            for (i = 0; i < items.length; i++) {
+                list[i] = items[i];
+            }
+            items = list;
+
+        }
+        items[count] = item;
+        count++;
+    }
+
+    public void deleteElementatIndex(int index) {
+        boolean deleted = false;
+        if (index < 0 || index >= items.length) {
+            System.out.println("index out of bounds");
+        } else {
+            int[] list = new int[items.length-1];
+            for (i = 0; i < list.length; i++) {
+                if (i == index) {
+                    list[i] = items[i + 1];
+                    deleted = true;
+                } else if (deleted) {
+                    list[i] = items[i + 1];
+                } else {
+                    list[i] = items[i];
+                }
+            }
+            items = list;
+        }
+    }
+
+}
--- src/datastructures/DataStructures.java
+++ src/datastructures/DataStructures.java
@@ -3,11 +3,13 @@ package datastructures;
 public class DataStructures {
 
     public static void main(String[] args) {
-        Arrays num = new Arrays(3);
+        ArraysBuild num = new ArraysBuild(3);
         num.insertElement(10);
         num.insertElement(20);
         num.insertElement(30);
         num.insertElement(40);
+        
+        num.deleteElementatIndex(1);
         num.printArray();
     }
 }
